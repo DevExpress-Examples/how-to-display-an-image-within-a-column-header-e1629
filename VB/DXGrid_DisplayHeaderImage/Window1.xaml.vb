fd2ee03ccd55139a17b6e982c7a3f71213ca7dc5
DevExpressExampleBot <fakeEmail>
2022-06-13 15:16:52 +0400

    Source auto update [skip ci]

--- VB/DXGrid_DisplayHeaderImage/Window1.xaml.vb
+++ VB/DXGrid_DisplayHeaderImage/Window1.xaml.vb
@@ -1,69 +1,36 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
+Imports System.Collections.ObjectModel
 Imports System.Windows
-Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace DXGrid_DisplayHeaderImage
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = Products.GetData()
-		End Sub
-	End Class
-	Public Class Products
-		Public Shared Function GetData() As List(Of Product)
-			Dim data As New List(Of Product)()
-			data.Add(New Product() With {.ProductName = "Chai", _
-				.UnitPrice = 18, .UnitsOnOrder = 10})
-			data.Add(New Product() With {.ProductName = "Ipoh Coffee", _
-				.UnitPrice = 36.8, .UnitsOnOrder = 12})
-			data.Add(New Product() With {.ProductName = "Outback Lager", _
-				.UnitPrice = 12, .UnitsOnOrder = 25})
-			data.Add(New Product() With {.ProductName = "Boston Crab Meat", _
-				.UnitPrice = 18.4, .UnitsOnOrder = 18})
-			data.Add(New Product() With {.ProductName = "Konbu", _
-				.UnitPrice = 6, .UnitsOnOrder = 24})
-			Return data
-		End Function
-	End Class
-	Public Class Product
-		Private privateProductName As String
-		Public Property ProductName() As String
-			Get
-				Return privateProductName
-			End Get
-			Set(ByVal value As String)
-				privateProductName = value
-			End Set
-		End Property
-		Private privateUnitPrice As Double
-		Public Property UnitPrice() As Double
-			Get
-				Return privateUnitPrice
-			End Get
-			Set(ByVal value As Double)
-				privateUnitPrice = value
-			End Set
-		End Property
-		Private privateUnitsOnOrder As Integer
-		Public Property UnitsOnOrder() As Integer
-			Get
-				Return privateUnitsOnOrder
-			End Get
-			Set(ByVal value As Integer)
-				privateUnitsOnOrder = value
-			End Set
-		End Property
-	End Class
+
+    Public Partial Class Window1
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = Products.GetData()
+        End Sub
+    End Class
+
+    Public Class Products
+
+        Public Shared Function GetData() As ObservableCollection(Of Product)
+            Dim data As ObservableCollection(Of Product) = New ObservableCollection(Of Product)()
+            data.Add(New Product() With {.ProductName = "Chai", .UnitPrice = 18, .UnitsOnOrder = 10})
+            data.Add(New Product() With {.ProductName = "Ipoh Coffee", .UnitPrice = 36.8, .UnitsOnOrder = 12})
+            data.Add(New Product() With {.ProductName = "Outback Lager", .UnitPrice = 12, .UnitsOnOrder = 25})
+            data.Add(New Product() With {.ProductName = "Boston Crab Meat", .UnitPrice = 18.4, .UnitsOnOrder = 18})
+            data.Add(New Product() With {.ProductName = "Konbu", .UnitPrice = 6, .UnitsOnOrder = 24})
+            Return data
+        End Function
+    End Class
+
+    Public Class Product
+
+        Public Property ProductName As String
+
+        Public Property UnitPrice As Double
+
+        Public Property UnitsOnOrder As Integer
+    End Class
 End Namespace
